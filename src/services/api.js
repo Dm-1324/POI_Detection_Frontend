ec6661a33909f387f12be4a28843f7365b6e489d
Dhruv Malik <dhruvm132@gmail.com>
2026-04-16 12:17:28 +0530

    render link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://poidetection-backend.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
